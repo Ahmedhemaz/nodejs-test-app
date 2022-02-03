2777bb322c8b4cda629d012be417cd6f1db20de8
Ahmedhemaz <ah_ibrahim94@hotmail.com>
2022-02-03 11:24:30 +0200

    set paths of local ref

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 import express, { Application, Request, Response } from "express";
-
+import { hello } from "tsloggy";
 const app: Application = express();
 
 const port: number = 3001;
 
 app.get("/toto", (req: Request, res: Response) => {
+  hello();
   res.send("Hello toto");
 });
 
